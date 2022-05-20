b09729fba40f980d3a0d6c9ad5615c0413b55772
Timotej979 <timotej.pet@gmail.com>
2022-05-20 14:00:05 +0200

    Update TaylorSeries.c
    Translation to english

--- C-programing-language/TaylorSeries.c
+++ C-programing-language/TaylorSeries.c
@@ -22,24 +22,24 @@ int main()
 
 
     while(1){
-        printf("\n  Taylorjeva vrsta za funkcijo:\n    -e^x (1)\n    -sin(x) (2)\n    -cos(x) (3)\n    -log(1 + x) razvit v okolici stevila 0 (4)\n    -Izhod iz programa (5)\n------------------------------\n  Vpisi svojo izbiro:\n   ");
+        printf("\n  Taylorjeva series for function:\n    -e^x (1)\n    -sin(x) (2)\n    -cos(x) (3)\n    -log(1 + x) developed around number 0 (4)\n    -Exit the program (5)\n------------------------------\n  Input your pick:\n   ");
         scanf("%d", &stikalo);
 
         if(stikalo != 5){
-            printf("  Vpisi okoli katerega x se razvije vrsta: \n   ");
+            printf("  Around which x should the series be developed: \n   ");
             scanf("%lf", &x);
 
 
-            printf("  Koliko clenov Taylorjeve vrste izracunam:\n    -10^n (1)\n    -poljubno stevilo (2)\n   ");
+            printf("  How many Taylor series parts should be calculated:\n    -10^n (1)\n    -Input a number of parts (2)\n   ");
             scanf("%d", &vrsta);
 
 
             if(vrsta == 1){
-                printf("  Vpisi eksponent 10-ke:\n   ");
+                printf("  Input exponent 10-ke:\n   ");
                 scanf("  %d", &deset);
                 cleni = pow(10, deset);
             } else if(vrsta == 2){
-                printf("  Vpisi poljubno stevilo clenov:\n   ");
+                printf("  Input a number of parts:\n   ");
                 scanf("  %lf", &cleni);
             }
         }
@@ -58,7 +58,7 @@ int main()
                 logarithm(x, cleni);
                 break;
             case 5:
-                printf("  Hvala za uporabo programa.");
+                printf("  Thanks for using my program.");
                 return 0;
         }
     }
@@ -74,7 +74,7 @@ void eksponent(double x, double cleni){
     }
     quadmath_snprintf(str, sizeof str, "%.34Qf", result);
 
-    printf("  Priblizek e^x okoli tocke %g z %g cleni je:\n", x, cleni);
+    printf("  Aproximation e^x around point %g with %g part is:\n", x, cleni);
     printf("  %s\n--------------------------------------------------", str);
     result = 0.0q;
 }
@@ -89,7 +89,7 @@ void sinus(double x, double cleni){
     }
     quadmath_snprintf(str, sizeof str, "%.34Qf", result);
 
-    printf("  Priblizek sin(x) okoli tocke %g z %g cleni je:\n", x, cleni);
+    printf("  Aproximation sin(x) around point %g with %g parts is:\n", x, cleni);
     printf("  %s\n--------------------------------------------------", str);
     result = 0.0q;
 }
@@ -104,7 +104,7 @@ void cosinus(double x, double cleni){
     }
     quadmath_snprintf(str, sizeof str, "%.34Qf", result);
 
-    printf("  Priblizek cos(x) okoli tocke %g z %g cleni je:\n", x, cleni);
+    printf("  Aproximation cos(x) around point %g with %g parts is:\n", x, cleni);
     printf("  %s\n--------------------------------------------------", str);
     result = 0.0q;
 }
@@ -117,7 +117,7 @@ void logarithm(double x, double cleni){
     }
     quadmath_snprintf(str, sizeof str, "%.34Qf", result);
 
-    printf("  Priblizek log(1 + x) okoli tocke %g z %g cleni je:\n", x, cleni);
+    printf("  Aproximation log(1 + x) around point %g with %g parts is:\n", x, cleni);
     printf("  %s\n--------------------------------------------------", str);
     result = 0.0q;
 }
